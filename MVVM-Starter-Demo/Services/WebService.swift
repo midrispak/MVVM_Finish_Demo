e5e49a0083a65c25303cf5c47454fae48015b219
Muhammad Idris <xs2idris@gmail.com>
2019-04-29 00:42:03 +0500

    date changed.

--- MVVM-Starter-Demo/Services/WebService.swift
+++ MVVM-Starter-Demo/Services/WebService.swift
@@ -19,7 +19,7 @@ class WebService {
         
         let ageOfAltron = Movie.init(movieTitle: "Avengers: Age of Ultron", ratings: 4.0, releaseDateString: "04/23/2015")
         
-        let infinityWar = Movie.init(movieTitle: "Avengers: Infinity War", ratings: 8.2, releaseDateString: "04/26/2019")
+        let infinityWar = Movie.init(movieTitle: "Avengers: Infinity War", ratings: 8.2, releaseDateString: "04/26/2018")
         
         let endGame = Movie.init(movieTitle: "Avengers - Endgame", ratings: 7.9, releaseDateString: "04/26/2019")
         
